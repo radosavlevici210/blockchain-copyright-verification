dc1ad46c3ed424e429f380514842a5ff82384117
Ervin Remus Radosavlevici <radosavlevici210@icloud.com>
2025-06-05 23:30:28 +0100

    Production .eslintrc.js configuration

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
+
+// Copyright © Ervin Remus Radosavlevici (radosavlevici210@icloud.com)
